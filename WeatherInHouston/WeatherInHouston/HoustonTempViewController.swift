56a280e165d63c261c0bad0d3011be664cd9b4cd
hxqlin <hxqlin@gmail.com>
2017-08-25 10:42:09 -0700

    Finished task

---
+++ WeatherInHouston/WeatherInHouston/HoustonTempViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HoustonTempViewController.swift
+//  WeatherInHouston
+//
+//  Created by Hannah Lin on 2017-08-24.
+//  Copyright © 2017 Hannah Lin. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class HoustonTempViewController: UIViewController {
+    
+    @IBOutlet weak var temperatureDisplayField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let apiToContact = "http://api.openweathermap.org/data/2.5/weather?q=Houston&APPID=73c185f18ad3f1415c96f34fafadddf6"
+        Alamofire.request(apiToContact).validate().responseJSON() { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    
+                    // value in JSON file is in Kelvin
+                    let tempInHoustonInKelvin = json["main"]["temp"].doubleValue
+                    
+                    // to convert Kelvin to Celsius, subtract 273.15
+                    let tempInHoustonInCelsius = tempInHoustonInKelvin - 273.15
+                    
+                    // change text to display temperature in Houston
+                    self.temperatureDisplayField.text = "\(tempInHoustonInCelsius) \u{00B0}C"
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+
+
+
+}
---
+++ WeatherInHouston/WeatherInHouston/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherInHouston
+//
+//  Created by Hannah Lin on 2017-08-24.
+//  Copyright © 2017 Hannah Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        }
+
+
+
+}
+
